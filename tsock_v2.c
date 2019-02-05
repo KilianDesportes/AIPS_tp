8dc04e38896361a4d2917b7c61cdcd5274b12f94
kde <kilian.desportes@gmail.com>
2019-02-05 19:52:20 +0100

    modif v2

--- tsock_v2.c
+++ tsock_v2.c
@@ -15,15 +15,6 @@ void construire_message(char * message, char motif, int lg){
 	}
 }
 
-void afficher_message(char * message, int lg){
-	int j;
-	printf("Message construit : ");
-	for(j = 0 ; j < lg ; j++){
-		printf("%c",message[j]);
-	}
-	printf("\n");
-}
-
 int main (int argc, char **argv)
 {
 	int c;
@@ -36,10 +27,9 @@ int main (int argc, char **argv)
 	int nb_port = -1; /* Numéro de port */
 	int nb_port_htons = -1; /* Numéro de port big endian */
 	char * host_name; /* Nom d'hôte */
-
+	int soc; /* Socket */
+	char * proc; /* Nom du protocole */
 	// Partie INITIALISATION DES PARAMETRES
-
-
 	while ((c = getopt(argc, argv, "psu")) != -1) {
 		switch (c) {
 		case 'p':
@@ -49,63 +39,39 @@ int main (int argc, char **argv)
 			}
 			source = 0;
 			break;
-
 		case 's':
 			if (source == 0) {
 				printf("usage: tsock [-p|-s] [-u] [-n ##] [-l ##] [host] port\n");
 				exit(1) ;
 			}
 			source = 1;
+			host_name = argv[argc-2];
 			break;
-
-		
 		case 'l':
 			len_message = atoi(optarg);
 			break;
-		
-
 		case 'u':
 			protocole = 1;
 			break;
-
-		
 		case 'n':
 			nb_message = atoi(optarg);
 			break;
-		
 		default:
 			printf("usage: tsock [-p|-s] [-u] [-n ##] [-l ##] [host] port\n");
 			break;
 		}
 	}
-
-
 	if (source == -1) {
 		printf("usage: tsock [-p|-s] [-u] [-n ##] [-l ##] [host] port\n");
 		exit(1) ;
 	}
-
 	nb_port = atoi(argv[argc-1]);
 	nb_port_htons = htons(nb_port);
-
 	if (nb_port == -1){
 		printf("usage: tsock [-p|-s] [-u] [-n ##] [-l ##] port\n");
 		exit(1);
 	}
-
-	if (source == 1){
-		host_name = argv[argc-2];
-	}
-
-	if (source == 1) {
-		nb_message = 10 ;
-	}
-
 	// Partie EMISSION DU MESSAGE
-
-	int sock;
-	char * proc;
-
 	if(protocole == 1){
 		if((sock = socket(AF_INET,SOCK_DGRAM,0)) == -1){
 			perror("Echec de création du socket\n");
@@ -121,32 +87,24 @@ int main (int argc, char **argv)
 			proc = "TCP";
 		}
 	}
-
+	// RECEPTION - PUIT
 	if(source == 0){
-		//reception
-
-		if(protocole == 1){
-			struct sockaddr_in adr_local;
-
-			memset((char *)&adr_local, 0, sizeof(adr_local));
-
-			adr_local.sin_family = AF_INET;
-			adr_local.sin_port = nb_port;
-			adr_local.sin_addr.s_addr = INADDR_ANY;
-
-			int lg_adr_local = sizeof(adr_local);
-
-			if(bind(sock,(struct sockaddr *)&adr_local, lg_adr_local) == -1){
+		struct sockaddr_in adr_local;
+		memset((char *)&adr_local, 0, sizeof(adr_local));
+		adr_local.sin_family = AF_INET;
+		adr_local.sin_port = nb_port;
+		adr_local.sin_addr.s_addr = INADDR_ANY;
+		int lg_adr_local = sizeof(adr_local);
+		if(bind(sock,(struct sockaddr *)&adr_local, lg_adr_local) == -1){
 				perror("Echec du bind\n");
 				exit(1);
-			}
-
+		}
+		char * msg = malloc(sizeof(char)*len_message);
+		int i = 1;
+		// RECEPTION TCP
+		if(protocole == 0){
 			int sock_bis;
-			char * msg = malloc(sizeof(char)*len_message);
-			int i = 1;
 			printf("PUITS : lg_mesg-lu=%d, port=%d, nb_reception=infini, TP=%s\n",len_message,nb_port,proc);
-
-
 			while(1){
 				if((sock_bis=accept(sock,(struct sockaddr *)&adr_local,&lg_adr_local)) == -1){
 					//ECHEC ACCEPT
@@ -157,35 +115,12 @@ int main (int argc, char **argv)
 				}
 				}
 			}
-
 		}else{
-
-			struct sockaddr_in adr_local;
-
-			memset((char *)&adr_local, 0, sizeof(adr_local));
-
-			adr_local.sin_family = AF_INET;
-			adr_local.sin_port = nb_port;
-			adr_local.sin_addr.s_addr = INADDR_ANY;
-
-			int lg_adr_local = sizeof(adr_local);
-
-			if(bind(sock,(struct sockaddr *)&adr_local, lg_adr_local) == -1){
-				perror("Echec du bind\n");
-				exit(1);
-			}
-
-			char * msg = malloc(sizeof(char)*len_message);
-
-			int size_adr_local = sizeof(adr_local);
-
+			// RECEPTION UDP
 			struct sockaddr_in adr_distant;
-
 			memset((char *)&adr_distant, 0, sizeof(adr_distant));
-
+			int size_adr_local = sizeof(adr_local);
 			int lg_adr_distant = sizeof(adr_distant);
-
-			int i = 1;
 			printf("PUITS : lg_mesg-lu=%d, port=%d, nb_reception=infini, TP=%s\n",len_message,nb_port,proc);
 			while(1){
 				if(recvfrom(sock,msg,len_message,0,(struct sockaddr *)&adr_distant,&lg_adr_distant)!=-1){
@@ -194,35 +129,25 @@ int main (int argc, char **argv)
 				}
 			}
 		}
-		
-
-
-
 	}else{
-		//envoi
-		if(protocole == 1){
-			struct hostent *hp;
-			struct sockaddr_in adr_distant;
-
-			memset((char *)&adr_distant, 0, sizeof(adr_distant));
-
-			adr_distant.sin_family = AF_INET;
-			adr_distant.sin_port = nb_port;
-
-
-			if((hp = gethostbyname(host_name)) == NULL){
+		// ENVOI - SOURCE
+		struct hostent *hp;
+		struct sockaddr_in adr_distant;
+		memset((char *)&adr_distant, 0, sizeof(adr_distant));
+		adr_distant.sin_family = AF_INET;
+		adr_distant.sin_port = nb_port;
+		if((hp = gethostbyname(host_name)) == NULL){
 				perror("Erreur gethostbyname");
 				exit(1);
-			}
-
-			memcpy((char*)&(adr_distant.sin_addr.s_addr),
+		}
+		memcpy((char*)&(adr_distant.sin_addr.s_addr),
 				hp->h_addr,
 				hp->h_length);
 
-			int nb_char_sent;
-			int i;
+		int nb_char_sent, i;
+		if(protocole == 1){
+			//ENVOI UDP
 			printf("SOURCE : port=%d, nb_reception=infini, nb_envois=%d, TP=%s, dest=%s\n",nb_port,nb_message,proc,host_name);
-
 			for(i = 0 ; i < nb_message ; i++){
 				char * msg = malloc(sizeof(char)*len_message);
 				construire_message(msg,'a',len_message);
@@ -231,34 +156,12 @@ int main (int argc, char **argv)
 			}
 			printf("SOURCE : Fin\n");
 		}else{
-			struct hostent *hp;
-			struct sockaddr_in adr_distant;
-
-			int lg_adr_distant = sizeof(adr_distant);
-
-			memset((char *)&adr_distant, 0, sizeof(adr_distant));
-
-			adr_distant.sin_family = AF_INET;
-			adr_distant.sin_port = nb_port;
-
-			if((hp = gethostbyname(host_name)) == NULL){
-				perror("Erreur gethostbyname");
-				exit(1);
-			}
-
-			memcpy((char*)&(adr_distant.sin_addr.s_addr),
-				hp->h_addr,
-				hp->h_length);
-
-
+			//ENVOI TCP
 			if(connect(sock,(struct sockaddr *)&adr_distant,lg_adr_distant) != 0){
 				perror("Erreur connect");
 				exit(1);
 			}else{
-				int nb_char_sent;
-				int i;
 				printf("SOURCE : port=%d, nb_reception=infini, nb_envois=%d, TP=%s, dest=%s\n",nb_port,nb_message,proc,host_name);
-
 				for(i = 0 ; i < nb_message ; i++){
 					char * msg = malloc(sizeof(char)*len_message);
 					construire_message(msg,'a',len_message);
@@ -267,15 +170,7 @@ int main (int argc, char **argv)
 				}
 				printf("SOURCE : Fin\n");
 			}
-
 		}
-		
-
-		
-
 	}
-
 	return 0;
-
-
 }
