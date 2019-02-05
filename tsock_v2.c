5b317c5d899f0eb9808db6f27fff5d0e680c5b03
kde <kilian.desportes@gmail.com>
2019-02-05 20:09:55 +0100

    modif v2

--- tsock_v2.c
+++ tsock_v2.c
@@ -48,15 +48,13 @@ int main (int argc, char **argv)
 			host_name = argv[argc-2];
 			break;
 		case 'l':
-			printf("%s\n",optarg);
-			len_message = atoi(optarg);
+			len_message = atoi(&optarg);
 			break;
 		case 'u':
 			protocole = 1;
 			break;
 		case 'n':
-			printf("%s\n",optarg);
-			nb_message = atoi(optarg);
+			nb_message = atoi(&optarg);
 			break;
 		default:
 			printf("usage: tsock [-p|-s] [-u] [-n ##] [-l ##] [host] port\n");
